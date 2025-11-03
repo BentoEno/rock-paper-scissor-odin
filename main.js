796b2465d30230ed5c2068b9d7f97b411eb7b742
Bento Eno <aabid6000@gmail.com>
2025-11-03 21:21:59 +0900

    I solve the project
    I am sorry Mr.Odin, I forgot to commit, this poor
    will try his best in the future to not repeat
    the same mistake again

---
+++ main.js
@@ -0,0 +1,68 @@
+console.log('Hello World!');
+/ GET computer choice: rock, paper, or scissor / 
+function getComputerChoice () {
+    let choice = Math.floor(Math.random() * 3);
+    switch (choice) {
+        case 0:
+            choice = 'rock';
+            break;
+        case 1:
+            choice = 'paper';
+            break;
+        case 2:
+            choice = 'scissor';
+            break;
+        default:
+            choice = 'invalid choice'
+    }
+    return choice;
+}
+
+/ GET human choice: rock, paper, or scissor / 
+function getHumanChoice() {
+    let choice = prompt(`What is your choice? (type: rock, paper, or scissor)`);
+    if (choice.toLowerCase() === 'rock' || choice.toLowerCase() === 'paper' || choice.toLowerCase() === 'scissor') {
+        choice = choice.toLowerCase();
+    } else {
+        choice = 'invalid choice';
+    }
+    return choice;
+}
+
+/*  
+    IF human win: add point to human
+    ELSE IF computer win: add point to computer
+    ELSE the point not change
+*/ 
+
+
+/* IF either side gets to 5 points first, the other side lose */ 
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+const playRound = (humanChoice, computerChoice) => {
+    if (humanChoice === 'rock' && computerChoice === 'scissor' || humanChoice === 'paper' && computerChoice === 'rock' || humanChoice === 'scissor' && computerChoice === 'paper') {
+        ++humanScore;
+        console.log('You Win!, you beat the stupid computer');
+        alert('You Win!, you beat the stupid computer');
+    } else if (humanChoice === computerChoice) {
+        console.log(`It's a Tie! Try Again`);
+        alert(`It's a Tie! Try Again`);
+    } else {
+        ++computerScore;
+        console.log(`You Lose!, the stupid computer beat you!`);
+        alert(`You Lose!, the stupid computer beat you!`);
+    }
+}
+while (humanScore <= 5 && computerScore <= 5) {
+    playRound(getHumanChoice(), getComputerChoice());
+}
+
+if (humanScore = 5) {
+    console.log('You Won The Game, All Hail Human Race!');
+} else {
+    console.log('You Lose..., What a disappointment to Human Race!');
+}
+}
